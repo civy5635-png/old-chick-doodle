7505457298d8fd5e724c5ff3f326ca4dd3bc7a00
Carla Ivy <civy5635@gmail.com>
2026-06-22 16:20:22 -0500

    added js and css for theme nav to work

--- HTML-project-pages/script.js
+++ HTML-project-pages/script.js
@@ -10,3 +10,44 @@ document.addEventListener("DOMContentLoaded", function () {
     }
 
 });
+
+const lightMode = document.querySelector("#lightMode");
+const darkMode = document.querySelector("#darkMode");
+const smallFont = document.querySelector("#smallFont");
+const mediumFont = document.querySelector("#mediumFont");
+const largeFont = document.querySelector("#largeFont");
+
+if (lightMode) {
+    lightMode.addEventListener("click", function () {
+        document.body.classList.remove("dark-mode");
+        document.body.classList.add("light-mode");
+    });
+}
+
+if (darkMode) {
+    darkMode.addEventListener("click", function () {
+        document.body.classList.remove("light-mode");
+        document.body.classList.add("dark-mode");
+    });
+}
+
+if (smallFont) {
+    smallFont.addEventListener("click", function () {
+        document.body.classList.remove("font-medium", "font-large");
+        document.body.classList.add("font-small");
+    });
+}
+
+if (mediumFont) {
+    mediumFont.addEventListener("click", function () {
+        document.body.classList.remove("font-small", "font-large");
+        document.body.classList.add("font-medium");
+    });
+}
+
+if (largeFont) {
+    largeFont.addEventListener("click", function () {
+        document.body.classList.remove("font-small", "font-medium");
+        document.body.classList.add("font-large");
+    });
+}
