6fc77a366901571630d759ee6a4c85de2622617a
Carla Ivy <civy5635@gmail.com>
2026-06-22 14:59:15 -0500

    hamburger hell, still. changing js

--- script.js
+++ script.js
@@ -1,9 +1,12 @@
 document.addEventListener("DOMContentLoaded", function () {
+    alert("JavaScript is loaded");
+
     const menuButton = document.querySelector(".menu-toggle");
     const sidebar = document.querySelector(".sidebar");
 
     if (menuButton && sidebar) {
         menuButton.addEventListener("click", function () {
+            alert("Hamburger clicked");
             sidebar.classList.toggle("nav-open");
         });
     }
