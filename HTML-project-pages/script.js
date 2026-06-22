5914a594ebba6f7904ccc5869a5f3e4e71df58d5
Carla Ivy <civy5635@gmail.com>
2026-06-22 16:05:07 -0500

    finally got it, deleted extra js file

--- HTML-project-pages/script.js
+++ HTML-project-pages/script.js
@@ -1,15 +1,10 @@
 document.addEventListener("DOMContentLoaded", function () {
 
-    alert("JavaScript loaded");
-
     const menuButton = document.querySelector(".menu-toggle");
     const sidebar = document.querySelector(".sidebar");
 
     if (menuButton && sidebar) {
         menuButton.addEventListener("click", function () {
-
-            alert("Hamburger clicked");
-
             sidebar.classList.toggle("nav-open");
         });
     }
